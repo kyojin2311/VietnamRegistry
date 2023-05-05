27047df8719d7b1a0f8b38a829a1cde7b0a99ab1
Pham Van Thach <21020239@vnu.edu.vn>
2023-05-05 17:22:44 +0700

    fetch

--- client/app/routes/_index.jsx
+++ client/app/routes/_index.jsx
@@ -1,29 +1,44 @@
 import DOMPurify from "dompurify";
 import Login from "../components/Login";
 import styles from "../styles/LoginBox.css";
+import { redirect } from "@remix-run/node";
 export const meta = () => {
   return [{ title: "New Remix App" }];
 };
 
 export default function Index() {
-  return <Login />
+  return <Login />;
 }
 
-export async function action ({request}) {
+export async function action({ request }) {
   const formData = await request.formData();
-  const data = Object.fromEntries(formData);
-  const sendData = {
-    email: DOMPurify.sanitize(data.email),
-    password: DOMPurify.sanitize(data.password)
-  }
-  const response = await fetch()
+  const data = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
+  // const sendData = {
+  //   email: DOMPurify.sanitize(data.email),
+  //   password: DOMPurify.sanitize(data.password)
+  // }
+  const response = await fetch(
+    "https://sleepy-coast-93816.herokuapp.com/api/v1/users/login",
+    {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(data),
+    }
+  );
+  const resData = await response.json();
+  console.log(resData);
+  if (response.ok) return redirect("/home");
+  else return redirect("/");
 }
 
 export function links() {
-  return[
+  return [
     {
-      rel: 'stylesheet',
-      href: styles
-    }
-  ]
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
 }
