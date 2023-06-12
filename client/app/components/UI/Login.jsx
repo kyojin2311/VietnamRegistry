689f4dfeb87bfd9157b45b2f99f925beeab2358d
Pham Van Thach <21020239@vnu.edu.vn>
2023-06-12 20:28:39 +0700

    end

--- client/app/components/UI/Login.jsx
+++ client/app/components/UI/Login.jsx
@@ -65,14 +65,14 @@ export default function Login() {
               <button className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                 Sign in
               </button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+              {/* <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 <Link
                   to="/forgot-pass"
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >
                   Forgot Password
                 </Link>
-              </p>
+              </p> */}
             </Form>
           </div>
         </div>
