eff4a06090fb8f2801f3d60a17a25eb267cfb782
Pham Van Thach <21020239@vnu.edu.vn>
2023-06-12 20:24:16 +0700

    final

--- client/app/components/Inspections/InspectionsForm.jsx
+++ client/app/components/Inspections/InspectionsForm.jsx
@@ -71,7 +71,7 @@ export default function InspectionsForm({ data }) {
                     />
                   </span>
                 </div>
-                <div className="flex-initial ms-3">
+                {/* <div className="flex-initial ms-3">
                   Cải tạo
                   <span className="italic text-sm">
                     (Modification)
@@ -81,7 +81,7 @@ export default function InspectionsForm({ data }) {
                       className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                     />
                   </span>
-                </div>
+                </div> */}
               </div>
             </section>
             <section className="m-4">
